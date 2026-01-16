7491064783901e1623360e6600acf09ec1a7dd0a
Sihle-eng <smchobokazi@byupathway.edu>
2026-01-16 12:25:24 +0200

    Fix vite config entry paths

--- vite.config.js
+++ vite.config.js
@@ -8,9 +8,9 @@ export default defineConfig({
     outDir: "../dist",
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "src/index.html"),
-        cart: resolve(__dirname, "src", "cart", "index.html"),
-        checkout: resolve(__dirname, "src", "checkout", "index.html"),
+        main: "index.html",
+        cart: "cart/index.html",
+        checkout: "checkout/index.html",
       },
     },
   },
